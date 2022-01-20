98892b15f9ffbdeb7901e684ef7aa39b3c146a40
Andy Mina <andy.mina@macaulay.cuny.edu>
2022-01-19 22:52:53 -0500

    feat: add sprites/textures

--- js/index.js
+++ js/index.js
@@ -1,20 +1,38 @@
+// special syntax highlighting for keywords
+CodeMirror.defineOption("keyword", {}, function(cm, val, prev) {
+  if (prev == CodeMirror.Init) prev = false;
+  if (prev && !val)
+    cm.removeOverlay("keyword");
+  else if (!prev && val)
+    cm.addOverlay({
+      token: function(stream) {
+        for (var key in cm.options.keyword) {
+          if (stream.match(key, true)) {return cm.options.keyword[key];}
+        }
+        stream.next();
+      },
+      name: "keyword"
+    });
+});
+
 let editor = CodeMirror.fromTextArea($("#code-editor")[0], {
   lineNumbers: true,
-  value: "// Guide the rover!",
   theme: "base16-dark",
   mode: "javascript",
   showCursorWhenSelecting: true,
   autoCloseBrackets: true,
-  viewportMargin: Infinity
+  viewportMargin: Infinity,
+  keyword: {
+    "moveUp": "style1",
+    "moveDown": "style1",
+    "moveLeft": "style1",
+    "moveRight": "style1"
+  }
 });
 
-let msg = "Hello world!";
-
 const executeCode = () => {
-  // get CodeMirror text
-  let code = editor.getValue();
   try {
-    Function(`"use strict";${code}`)();
+    Function(`"use strict";${editor.getValue()}`)();
   } catch (err) {
     $("#code-errors").text(err);
   }
