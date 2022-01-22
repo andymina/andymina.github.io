e9384ac0c9f89bcc2817905d08a39ca03928b85c
Andy Mina <andy.mina@macaulay.cuny.edu>
2022-01-22 17:35:39 -0500

    feat: add typewriter to homepage

---
+++ js/text-ani.js
@@ -0,0 +1,46 @@
+$("document").ready(() => {
+  let dataText = ["The future needs you."];
+
+  // writes a str in dataText
+  const typeWriter = (currentStr, text, i, cb, isRed) => {
+    if (i < text.length) {
+      if (text[i] === " ") {
+        currentStr += "&nbsp";
+      } else if (text[i] === "y") {
+        // flag red if y
+        isRed = true;
+        currentStr += `<em style="color: red;">` + text[i];
+      } else if (text[i] === ".") {
+        // turn off flag for .
+        isRed = false;
+        currentStr += `</em>` + text[i];
+      } else {
+        // add letter
+        currentStr += text[i];
+      }
+
+      // update the html
+      $(".typewriter").html(`${currentStr}${isRed ? "</em>" : ""}<span aria-hidden="true"></span>`);
+
+      // wait and recurse for next char
+      setTimeout(() => typeWriter(currentStr, text, i + 1, cb, isRed), 100);
+    } else if (typeof cb == "function") {
+      // execute cb after finishing str
+      setTimeout(cb, 700);
+    }
+  }
+
+  // start a typewriter animation for a text in the dataText array
+  const StartTextAnimation = (i, dataText) => {
+    // loop if we finish everything in dataText
+    if (typeof dataText[i] == 'undefined')
+       setTimeout(() => StartTextAnimation(0, dataText), 10000);
+
+    // start typewriter for the current string with cb as next string
+    if (i < dataText.length)
+      typeWriter("", dataText[i], 0, () => StartTextAnimation(i + 1, dataText), false);
+  }
+
+  // start the text animation
+  StartTextAnimation(0, dataText);
+});
